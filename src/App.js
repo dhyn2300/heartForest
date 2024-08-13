586aec078d399f8aa636ea29b013919685c3de6a
dhyn2300 <dhyn2300@gmail.com>
2024-08-13 10:56:30 +0900

    py

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold text-blue-500 text-center mt-10">"HelloWorld"(노수안);</h1>
+        <h1 className="text-3xl font-bold text-blue-500 text-center mt-10">"HelloWorld"(print);</h1>
       </header>
     </div>
   );
